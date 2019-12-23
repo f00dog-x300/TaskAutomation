5340d57c21d8bb3a1fb2be6db6db2a7c93ebcf11
Kevin Lao <f00dog.x300@gmail.com>
2019-12-23 15:39:47 -0600

    working on file mover tests

--- FileManagement/log_parser.py
+++ FileManagement/log_parser.py
@@ -2,9 +2,9 @@ import codecs
 import pathlib
 
 bad_path = pathlib.Path(
-    r"..\ExampleFiles\170001451SW_Pulled_12052019_143006_PST.log")
+    r"ExampleFiles\170001451SW_Pulled_12052019_143006_PST.log")
 good_path = pathlib.Path(
-    r"..\ExampleFiles\[Com COM26] (2019-12-20_133052).log")
+    r"ExampleFiles\[Com COM26] (2019-12-20_133052).log")
 
 
 def log_reader(filepath):
@@ -19,3 +19,9 @@ for line in log_reader(bad_path):
     except UnicodeEncodeError:
         line = line.encode("utf8")
         print(line)
+
+
+# solution for the UnicodeEncodeError : https://stackoverflow.com/questions/54664815/unicodeencodeerror-charmap-codec-cant-encode-character-ufeff-in-position
+
+# TODO: Include regex search for specific log levels (CRI, ERR, etc)
+# TODO: perhaps move the print section as it's own function
---
+++ Tests/FileManagementTests/file_mover.py
@@ -0,0 +1,11 @@
+import configparser
+
+
+
+
+
+# source for configparser: https://docs.python.org/3/library/configparser.html
+# sources for tmpdir for pytest: 
+# └> https://code-maven.com/temporary-files-and-directory-for-pytest
+# └> https://pytest.readthedocs.io/en/2.7.3/tmpdir.html
+
