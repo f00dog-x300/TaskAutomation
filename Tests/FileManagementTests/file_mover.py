5a60e1afa208aab53cbe3d2ed1181a4b6e8a0b6b
Kevin Lao <f00dog.x300@gmail.com>
2019-12-26 10:32:23 -0600

    adding Scratch file with an example configparser.ini file for future config development

--- Tests/FileManagementTests/file_mover.py
+++ Tests/FileManagementTests/file_mover.py
@@ -1,11 +1,2 @@
-import configparser
 
-
-
-
-
-# source for configparser: https://docs.python.org/3/library/configparser.html
-# sources for tmpdir for pytest: 
-# └> https://code-maven.com/temporary-files-and-directory-for-pytest
-# └> https://pytest.readthedocs.io/en/2.7.3/tmpdir.html
 
