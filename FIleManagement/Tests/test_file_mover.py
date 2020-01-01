f34b5852e2b4d9bd7f36528e886b0f75ff919c47
f00dog <f00dog.x300@gmail.com>
2020-01-01 15:30:08 -0600

    added todo for test_create_file

--- FIleManagement/Tests/test_file_mover.py
+++ FIleManagement/Tests/test_file_mover.py
@@ -1,15 +1,31 @@
-import os
+
 
 
 CONTENT = "content"
 
 def test_create_file(tmp_path):
+"""Testing how to move one file"""
+    # creating test directories
+    source_dir = tmp_path / "source"
+    destination_dir = tmp_path / "destination"
+    source_dir.mkdir()
+    destination_dir.mkdir()
 
-    # created test directory
-    test_dir = tmp_path / "source"
-    test_dir.mkdir()
+    # calling instance of FileMover
+    filemover = FileMover(source_dir, destination_dir)
+
     # creating the test file inside test directory
-    test_file = test_dir / "test_file.txt"
+    test_file = source_dir / "test_file.txt"
     test_file.write_text(CONTENT)
+
+    # testing moving file to the destination 
+    filemover(test_file, destination_dir)
+    file_dest_dir_state = destination_dir / "test_file.txt"
+
+    # TODO : Assert that the temporary file is in the destination folder
+    
+
+
+
     assert test_file.read_text() == CONTENT
 
