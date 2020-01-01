98d2de6177df2af86b54043a84d8dad7f360fe6f
f00dog <f00dog.x300@gmail.com>
2020-01-01 13:22:08 -0600

    added test to the FileManagementFolder and fixed .gitconfig

---
+++ FileManagement/test_file_mover.py
@@ -0,0 +1,10 @@
+from FileManagement.file_mover import FileMover
+import pytest
+import sys
+
+print(sys.path)
+
+class Test_FileMover:
+
+    def test_move_files_to_new_dir(self):
+        assert True
